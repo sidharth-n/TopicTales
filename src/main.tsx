ef95aebc8741a74a103cabacc9edc314dc753959
sidharth-n <sidharthnofficial@gmail.com>
2023-10-25 20:05:24 +0530

    convert to pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.debug("Service Worker registered:", registration);
+    })
+    .catch((registrationError) => {
+      console.debug("Service Worker registration failed:", registrationError);
+    });
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   /*   <React.StrictMode> */
   <App />
